eec5c6fe77a62ac0d0779436625ade1c73825bf3
Vasanthan thamilmani <vasanthan4u2005@gmail.com>
2024-12-16 21:30:28 +0530

    vasanth : good boy

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const dbconnect = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root123",
+    database: "empmanage"
+}
+);
+
+dbconnect.connect((err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("CONNECTED");
+    }
+});
+
+module.exports = dbconnect;
